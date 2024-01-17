a6090fcaa78b886b27c14a74daa7914a802a3bc0
Thanushi Peiris <thanushi.peiris@mcri.edu.au>
2024-01-17 12:18:13 +1100

    fix napari_sam.utils import issue

--- src/napari_sam/_widget.py
+++ src/napari_sam/_widget.py
@@ -12,14 +12,14 @@ from collections import deque, defaultdict
 import inspect
 from segment_anything import SamPredictor, build_sam_vit_h, build_sam_vit_l, build_sam_vit_b
 from segment_anything.automatic_mask_generator import SamAutomaticMaskGenerator
-from napari_sam.utils import normalize###############################
+from .utils import normalize###############################
 import torch
 from vispy.util.keys import CONTROL
 import copy
 import warnings
 from tqdm import tqdm
 from superqt.utils import qdebounced
-from napari_sam.slicer import slicer
+from .slicer import slicer
 import urllib.request
 from pathlib import Path
 import os
